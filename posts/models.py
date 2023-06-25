d6ea1187eca6846ab3f05eaeab388023a6af1f6e
Armin sh fatemi <arminsh610@gmail.com>
2023-06-25 16:53:11 +0330

    models has been completed. Post,Comment,Blogger.

--- posts/models.py
+++ posts/models.py
@@ -7,10 +7,12 @@ class PostModel(models.Model):
     blogger = models.ForeignKey(BloggerModel, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=50, )
     text = models.CharField(blank=True, )
+    description = models.CharField(max_length=35, )
     likes_count = models.IntegerField(default=0, )
     comment_count = models.IntegerField(default=0, )
     is_enable = models.BooleanField(default=True, )
-    created = models.DateTimeField(auto_now_add=True, )
+    created_time = models.DateTimeField(auto_now_add=True, )
+    updated_time = models.DateTimeField(auto_now=True, )
     photo_main = models.ImageField(upload_to='photos/posts/%Y/%m/%d/', )
     photo_1 = models.ImageField(upload_to='photos/posts/%Y/%m/%d/', blank=True)
     photo_2 = models.ImageField(upload_to='photos/posts/%Y/%m/%d/', blank=True)
@@ -19,3 +21,12 @@ class PostModel(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class CommentModel(models.Model):
+    post = models.ForeignKey(PostModel, on_delete=models.DO_NOTHING, )
+    text = models.TextField(blank=False, )
+    created_time = models.DateTimeField(auto_now_add=True, )
+
+    def __str__(self):
+        return self.text
